a9883d217eae63f21dcaaf8b239320a7e8dc61e0
Sarath VS <sarathvss65@gmail.com>
2021-04-02 14:39:58 +0530

    First commit after creating the navbar

---
+++ src/data/SliderData.js
@@ -0,0 +1,43 @@
+import ImageOne from '../images/house-1.jpeg'
+import ImageTwo from '../images/house-2.jpeg'
+import ImageThree from '../images/house-3.jpeg'
+import ImageFour from '../images/house-4.jpeg'
+
+
+export const SliderData = [
+    {
+        title: 'Luxury villa in Bali ,Indonasia',
+        price: '$4,280,000',
+        label: 'View Home',
+        path: '/home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,280,000',
+        label: 'View Home',
+        path: '/home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasa, California',
+        price: '$8,280,000',
+        label: 'View Home',
+        path: '/home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$7,280,000',
+        label: 'View Home',
+        path: '/home',
+        image: ImageFour,
+        alt: 'House'
+    }
+
+
+];
+export default SliderData
